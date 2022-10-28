a3e52bdb45888b75db8b7e81f5b54bf7cb9f4816
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-28 17:54:46 -0300

    chore(models): add Team model

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,29 @@
+import { Model, STRING, INTEGER} from 'sequelize';
+import db from '.';
+import Match from './Match';
+
+export default class Team extends Model {
+  id: number;
+  teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: STRING,
+}, {
+    underscored: true,
+    sequelize: db,
+    modelName: 'team',
+    timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeMatch' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayMatch' });
