3e1ecd3f8ea99e541126b9777b62652cb5086121
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-28 17:54:27 -0300

    chore(models): add Match model

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,32 @@
+import { Model, INTEGER, BOOLEAN} from 'sequelize';
+import db from '.';
+
+export default class Match extends Model{
+  id!: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: INTEGER,
+  homeTeamGoals: INTEGER,
+  awayTeam: INTEGER,
+  awayTeamGoals: INTEGER,
+  inProgress: BOOLEAN,
+
+}, {
+    underscored: true,
+    sequelize: db,
+    modelName: 'match',
+    timestamps: false,
+});
