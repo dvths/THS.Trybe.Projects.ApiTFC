a99ebe2f7152e108dd385374e28e2f1d48a7a9a1
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-29 16:04:48 -0300

    chore(login): adds validation to the password

--- app/backend/src/Controllers/LoginController.ts
+++ app/backend/src/Controllers/LoginController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 export class LoginController {
   public create(request: Request, response: Response): Response {
     if(!request.body.email) return response.status(400).json({ message: 'All fields must be filled',});
+    if(!request.body.password) return response.status(400).json({ message: 'All fields must be filled',});
 
     return response.status(200);
   }
