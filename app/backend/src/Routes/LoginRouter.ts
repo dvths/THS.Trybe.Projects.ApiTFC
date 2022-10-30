d53ea3c09a94946ba92d72e2f71266be41a5bee9
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-30 01:50:10 -0300

    chore(routes): apply dependency injection

--- app/backend/src/Routes/LoginRouter.ts
+++ app/backend/src/Routes/LoginRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { LoginService } from '../Services/LoginService';
 import { LoginController } from '../Controllers/LoginController';
 
 const router = Router();
-const loginController = new LoginController();
+const loginService = new LoginService();
+const loginController = new LoginController(loginService);
 
 router.post('/login', (req, res) => loginController.create(req, res));
 
