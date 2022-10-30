1db48542007cee3eeea5a5caf9636c2b3fde5059
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-30 01:49:25 -0300

    chore(controller): apply dependency inversion

--- app/backend/src/Controllers/LoginController.ts
+++ app/backend/src/Controllers/LoginController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
+import { ILoginService } from '../Interfaces/ILoginService';
 import { LoginService } from '../Services/LoginService';
 
 export class LoginController {
   private loginService: LoginService;
 
-  constructor() {
-    this.loginService = new LoginService();
+  constructor(loginService: ILoginService) {
+    this.loginService = loginService;
   }
 
   public create(request: Request, response: Response): Response {
