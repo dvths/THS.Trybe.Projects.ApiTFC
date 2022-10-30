70dfa63703ee8128c7e2223015b65df0ee9f274e
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-30 14:07:27 -0300

    chore(tests): adds conflict error test

--- app/backend/src/tests/LoginControler.test.ts
+++ app/backend/src/tests/LoginControler.test.ts
@@ -1,8 +1,11 @@
 import * as chai from 'chai';
+import * as sinon from 'sinon';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { Response } from 'superagent';
+import { Model } from 'sequelize';
+import User from '../database/models/User';
 
 chai.use(chaiHttp);
 
@@ -36,6 +39,28 @@ describe('POST /login', () => {
       });
     });
   });
+
+  describe('Quando o usuário já existe no banco de dados', () => {
+    let httpResponse: Response;
+
+    const user = { id: 1, email: 'any_email@email.com', password: 'any_pass' };
+
+    before(() => sinon.stub(Model, 'findOne').resolves(user as User))
+
+    after(() => sinon.restore());
+
+    it('Deve retornar um erro 409 e a mensagem adequada', async () => {
+      httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: 'exists@email.com', password: 'any_pass' });
+      expect(httpResponse.status).to.equal(409);
+      expect(httpResponse.body).to.deep.equal({
+        message: 'User already registered!',
+      });
+    });
+  });
+
   describe('Quando a requisição é feita com sucesso: ', () => {
     let httpResponse: Response;
     it('Deve retornar status 201', async () => {
