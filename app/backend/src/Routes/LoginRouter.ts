c4911979102640681c97eacba900deb86b80855f
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-30 23:18:21 -0300

    chore(routes): includes middleware for body validation

--- app/backend/src/Routes/LoginRouter.ts
+++ app/backend/src/Routes/LoginRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { LoginService } from '../Services/LoginService';
 import { LoginController } from '../Controllers/LoginController';
+import { validateBody } from '../Middlewares/ValidateBody';
 
 const router = Router();
 const loginService = new LoginService();
 const loginController = new LoginController(loginService);
 
-router.post('/login', (req, res) => loginController.create(req, res));
+router.post('/login', validateBody(['email', 'password']), (req, res) =>
+  loginController.login(req, res)
+);
 
 export { router as loginRouter };
