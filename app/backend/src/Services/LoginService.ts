baf1edbd55fc6fc930e4eadb8d35c3f3ffd76c73
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-30 23:17:16 -0300

    refactor(service): remove body validation for responsible middleware

--- app/backend/src/Services/LoginService.ts
+++ app/backend/src/Services/LoginService.ts
@@ -1,19 +1,11 @@
 import { ILogin, ILoginService } from '../Interfaces/ILoginService';
-import { LoginRequiredData } from '../Types/LoginRequiredData';
 import User from '../database/models/User';
-import { RequiredDataErrors } from '../Errors/RequiredDataErrors';
 import { ConflicDatatError } from '../Errors/ConflictDataErrors';
 
 export class LoginService implements ILoginService {
-
   public async login(login: ILogin): Promise<any> {
-    const requiredDataForLogin: LoginRequiredData = ['email', 'password'];
-    for (const data of requiredDataForLogin) {
-      if (!login[data])
-        throw new RequiredDataErrors('All fields must be filled');
-    }
 
-    const loginExists = await User.findOne({ where: {email: login.email} });
+    const loginExists = await User.findOne({ where: { email: login.email } });
     if (loginExists) throw new ConflicDatatError('User already registered!');
   }
 }
