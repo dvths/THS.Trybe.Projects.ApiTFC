f6b08d3a79ade3cf9a809d04c3720ec21d29e164
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-01 20:22:29 -0300

    feat(authentication): add validation login on the controller

--- app/backend/src/Controllers/LoginController.ts
+++ app/backend/src/Controllers/LoginController.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from 'express';
-import { ILoginService } from '../Interfaces/ILoginService';
-import { LoginService } from '../Services/LoginService';
+import { ILoginService } from '../Interfaces/Services/ILoginService';
 
 export class LoginController {
-  private loginService: LoginService;
+  private readonly loginService: ILoginService;
 
   constructor(loginService: ILoginService) {
     this.loginService = loginService;
   }
 
   public async login(request: Request, response: Response): Promise<Response> {
-      await this.loginService.login(request.body);
-      return response.sendStatus(201);
+    const result = await this.loginService.login(request.body);
+    return response.status(200).json(result);
+  }
+
+  public async validate(request: Request, response: Response): Promise<Response> {
+    const { authorization } = request.headers;
+    const result = this.loginService.validate(authorization);
+
+    return response.status(200).json(result);
   }
 }
