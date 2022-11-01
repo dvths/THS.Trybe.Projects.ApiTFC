915495deb35847c41e9c07a021fefc06d337b0d7
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-01 20:18:30 -0300

    feate(authentication): adds authentication service

--- app/backend/src/Services/LoginService.ts
+++ app/backend/src/Services/LoginService.ts
@@ -1,11 +1,42 @@
-import { ILogin, ILoginService } from '../Interfaces/ILoginService';
+import { compare } from 'bcryptjs';
+import { generateToken, verifyToken } from '../Utils/JasonWebToken';
+import { ILogin, ILoginService } from '../Interfaces/Services/ILoginService';
 import User from '../database/models/User';
 import { UnauthorizedError } from '../Errors/UnauthorizedError';
+import {
+  IToken,
+  ITokenDecodedRole,
+} from '../Interfaces/Auth/IToken';
 
 export class LoginService implements ILoginService {
-  public async login(login: ILogin): Promise<any> {
-    const user = await User.findOne({ where: { email: login.email } });
-    if (!user || user.password !== login.password)
-      throw new UnauthorizedError('Incorrect email or password');
+  constructor(private _model = User) {}
+
+  public async login(credentials: ILogin): Promise<IToken> {
+    const user = await this._model.findOne({
+      where: { email: credentials.email },
+    });
+
+    if (user) {
+      const isValidPassword = await compare(
+        credentials.password,
+        user.password
+      );
+      if (isValidPassword) {
+        const { id, username, role } = user;
+        const token = generateToken({ id, username, role });
+
+        return { token };
+      }
+    }
+    throw new UnauthorizedError('Incorrect email or password');
   }
+
+  validate = (authorization: string | undefined): ITokenDecodedRole => {
+    if (authorization) {
+      const { role } = verifyToken(authorization);
+      return { role };
+    }
+
+    throw new UnauthorizedError('Token must be a valid token');
+  };
 }
