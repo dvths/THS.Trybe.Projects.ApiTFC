7049d09262ae245d101ebf5e99bf949a8d8d38e5
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-02 11:02:00 -0300

    feat(login): add validate route to login

--- app/backend/src/Routes/LoginRouter.ts
+++ app/backend/src/Routes/LoginRouter.ts
@@ -9,7 +9,8 @@ const loginController = new LoginController(loginService);
 const router = Router();
 
 router.post('/login', loginMiddleware.validateFields, (req, res) =>
-  loginController.login(req, res)
-);
+  loginController.login(req, res));
+
+router.get('/login/validate', (req, res) => loginController.validate(req, res));
 
 export { router as loginRouter };
