52cf533b5216d917ca55992cad714f01a566d63d
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-02 11:01:05 -0300

    chore(tests): rename login tests file and add tests to authentication

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -1,12 +1,12 @@
 import * as chai from 'chai';
 import * as sinon from 'sinon';
-// import * as jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { Model } from 'sequelize';
 import User from '../database/models/User';
-import * as mock from './mocks-stubs';
+import * as mock from './mocks-stubs/login-mocks';
 
 chai.use(chaiHttp);
 
@@ -40,9 +40,9 @@ describe('POST /login', () => {
   });
 
   describe('Quando o email é informado e não existe no banco:', () => {
-    before(() => sinon.stub(Model, 'findOne').resolves(null));
+    beforeEach(() => sinon.stub(Model, 'findOne').resolves(null));
 
-    after(() => sinon.restore());
+    afterEach(() => sinon.restore());
 
     it('Deve retornar um erro 401 e a mensagem adequada', async () => {
       const { status, body } = await chai
@@ -57,11 +57,11 @@ describe('POST /login', () => {
   });
 
   describe('Quando o email informado existe no banco e a senha é incorreta:', () => {
-    before(() =>
+    beforeEach(() =>
       sinon.stub(Model, 'findOne').resolves(mock.login.data as User)
     );
 
-    after(() => sinon.restore());
+    afterEach(() => sinon.restore());
 
     it('Deve retornar um erro 401 e a mensagem adequada', async () => {
       const { status, body } = await chai
@@ -76,19 +76,56 @@ describe('POST /login', () => {
   });
 
   describe('Quando as credenciais estão corretas', () => {
-
-    before(() => {
+    beforeEach(() => {
       sinon.stub(Model, 'findOne').resolves(mock.login.requests.user as User);
     });
-    after(() => sinon.restore());
+    afterEach(() => sinon.restore());
 
     it('Deve retornar status 200 e um token no corpo da requisição', async () => {
       const { status, body } = await chai
-      .request(app)
-      .post('/login')
-      .send(mock.login.data);
+        .request(app)
+        .post('/login')
+        .send(mock.login.data);
       expect(status).to.equal(200);
       expect(body).to.have.key('token');
     });
   });
+});
+
+describe('GET /login/validate', () => {
+  describe('Quando o token não é enviado:', () => {
+    it('Deve retornar status 401 e a mensagem adequada', async () => {
+      const { status, body } = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', '');
+      expect(status).to.equal(401);
+      // expect(body).to.deep.equal({ message: 'Token must be a valid token' });
+      expect(body).to.deep.equal(mock.login.jwtAuth.errorMessage);
+    });
+  });
+  describe('Quando o token é invalido:', () => {
+    it('Deve retornar status 401 e a mensagem adequada', async () => {
+      const { status, body } = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', 'invalid_token');
+      expect(status).to.equal(401);
+      expect(body).to.deep.equal(mock.login.jwtAuth.errorMessage);
+    });
+  });
+  describe('Quando o token é válido', () => {
+    beforeEach(() => sinon.stub(jwt, 'verify').callsFake(() => {
+      return mock.login.jwtAuth.payload;
+    }))
+    afterEach(() => sinon.restore());
+    it('Deve retornar status 200 e objeto com a chave "role"', async ()=> {
+      const { status, body } = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', 'valid_token');
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal({ role: 'user' });
+    })
+  })
 });
