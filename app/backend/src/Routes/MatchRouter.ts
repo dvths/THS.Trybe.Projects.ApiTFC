9c725bc93888db62ab174afcfb348c7fda9b5c60
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-03 01:21:24 -0300

    chore(routes): add match route

--- app/backend/src/Routes/MatchRouter.ts
+++ app/backend/src/Routes/MatchRouter.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import { MatchController } from '../Controllers/MatchController';
 import { MatchService } from '../Services/MatchService';
-const router = Router();
 
 const matchService = new MatchService();
 const matchController = new MatchController(matchService);
+const router = Router();
 
-router.get('/matches', (_req, res) => matchController.get(_req, res));
+
+router.get('/matches', (_req, res) => matchController.getAll(_req, res));
 
 export {router as matchesRouter};
