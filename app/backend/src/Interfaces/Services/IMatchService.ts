7a6912a086745bbbbf064f2a46704f299c621126
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-03 11:08:23 -0300

    chore(matches): add in progress matches interface

--- app/backend/src/Interfaces/Services/IMatchService.ts
+++ app/backend/src/Interfaces/Services/IMatchService.ts
@@ -2,4 +2,5 @@ import { IMatches } from "../Matches/IMatch";
 
 export interface IMatchService {
   getAllMatches(): Promise<IMatches[]>
+  getAllMatchesInProgress(isInProgress: boolean): Promise<IMatches[]>
 }
