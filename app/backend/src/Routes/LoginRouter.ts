06f8e6385e053ea66798c3cd52e48e9cae57b6b2
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-02 22:03:18 -0300

    fix(routes): remove comments

--- app/backend/src/Routes/LoginRouter.ts
+++ app/backend/src/Routes/LoginRouter.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { LoginService } from '../Services/LoginService';
 import { LoginController } from '../Controllers/LoginController';
-// import { validateBody } from '../Middlewares/ValidateBody';
 import * as loginMiddleware from '../Middlewares/LoginMiddleware';
 
 const loginService = new LoginService();
