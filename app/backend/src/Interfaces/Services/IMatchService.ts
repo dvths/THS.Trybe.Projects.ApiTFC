28b254457d3a8bf7cd66f8a10577e3a604f57155
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-03 22:15:44 -0300

    feat(interfaces): add "create", "finished" adn "update" interfaces

--- app/backend/src/Interfaces/Services/IMatchService.ts
+++ app/backend/src/Interfaces/Services/IMatchService.ts
@@ -1,6 +1,9 @@
-import { IMatches } from "../Matches/IMatch";
+import { /*IMatch,*/ IMatchCreated, IMatches, IMatchFinished, IMatchUpdate } from "../Matches/IMatch";
 
 export interface IMatchService {
   getAllMatches(): Promise<IMatches[]>
   getAllMatchesInProgress(isInProgress: boolean): Promise<IMatches[]>
+  create(matchInfo: IMatchCreated): Promise<IMatchCreated>
+  finished(id: string | undefined): Promise<IMatchFinished>
+  update(id: string | undefined, userInfo: IMatchUpdate): Promise<void>
 }
