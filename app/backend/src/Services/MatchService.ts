67de82361cbe770962a768e5eefd4c78695b03b0
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-03 22:10:22 -0300

    chore(services): adds the "finished", "update" and "create" methods to the "match" services

--- app/backend/src/Services/MatchService.ts
+++ app/backend/src/Services/MatchService.ts
@@ -1,6 +1,7 @@
 import { IMatchService } from '../Interfaces/Services/IMatchService';
-import { IMatches } from '../Interfaces/Matches/IMatch';
+import { IMatchCreated, IMatches, IMatchFinished, IMatchUpdate } from '../Interfaces/Matches/IMatch';
 import { MatchesRepository } from '../Repositories/MatchesRepository/MatchesRepository';
+import { TeamService } from './TeamService';
 
 export class MatchService implements IMatchService {
   constructor(private repository = new MatchesRepository()) {}
@@ -11,5 +12,26 @@ export class MatchService implements IMatchService {
 
   public async getAllMatchesInProgress(isInProgress: boolean): Promise<IMatches[]> {
     return await this.repository.getMatchesInProgress(isInProgress);
+  }
+
+  public async create(matchInfo: IMatchCreated): Promise<IMatchCreated> {
+      const { homeTeam, awayTeam } = matchInfo;
+
+      const teamService = new TeamService();
+      await teamService.getById(`${homeTeam}`);
+      await teamService.getById(`${awayTeam}`);
+
+      return await this.repository.create({
+        ...matchInfo,
+        inProgress: true,
+      });
+    }
+
+  public async finished(id: string | undefined): Promise<IMatchFinished> {
+    return await this.repository.finished(id);
+  }
+
+  public async update(id: string | undefined, updateInfo: IMatchUpdate): Promise<void>{
+    return await this.repository.update(id, updateInfo);
   }
 }
