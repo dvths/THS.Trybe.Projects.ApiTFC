a8becd2d1b0a5600d13a7b42f942beace4603ad3
Tiago Henrique <tiagohs.dev@gmail.com>
2022-11-03 22:14:22 -0300

    feat(routes): add matches routes

--- app/backend/src/Routes/MatchRouter.ts
+++ app/backend/src/Routes/MatchRouter.ts
@@ -1,12 +1,22 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { MatchController } from '../Controllers/MatchController';
 import { MatchService } from '../Services/MatchService';
+import * as loginMiddleware from '../Middlewares/LoginMiddleware';
+import matchMiddleware from  '../Middlewares/MatchMiddleware';
 
 const matchService = new MatchService();
 const matchController = new MatchController(matchService);
 const router = Router();
 
+router.get('/matches', (req, res) => matchController.getAll(req, res));
 
-router.get('/matches', (_req, res) => matchController.getAll(_req, res));
+router.post('/matches', loginMiddleware.authentication, matchMiddleware, (req, res) =>
+  matchController.create(req, res),
+);
 
-export {router as matchesRouter};
+router.patch('/matches/:id', (req, res) => matchController.update(req, res));
+
+router.patch('/matches/:id/finish', (req, res) => matchController.finished(req, res));
+
+
+export { router as matchesRouter };
